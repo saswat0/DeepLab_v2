72adb14e1b1e62d8f3dcf0e08168c2dc72b06221
Saswat <saswatsubhajyotimallick@gmail.com>
2020-08-11 21:59:25 +0530

    additions

--- resnet.py
+++ resnet.py
@@ -32,3 +32,13 @@ class _ConvBnReLU(nn.Sequential):
         self.add_module("bn", _BATCH_NORM(out_ch, eps=1e-5, momentum=0.999))
         if relu:
             self.add_module("relu", nn.ReLU())
+
+
+if __name__ == "__main__":
+    model = ResNet(n_classes=1000, n_blocks=[3, 4, 23, 3])    
+    model.eval()
+    image = torch.randn(1, 3, 224, 224)
+
+    print(model)
+    print("input:", image.shape)
+    print("output:", model(image).size())
