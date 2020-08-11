5045d12dd9891c729561758e66f8cc14ed71d57c
Saswat <saswatsubhajyotimallick@gmail.com>
2020-08-11 21:57:51 +0530

    fixes

--- loss.py
+++ loss.py
@@ -5,3 +5,8 @@ class Loss(nn.Module):
     def __init__(self, args):
         super(Loss, self).__init__()
         self.args = args
+    
+    def forward(self, logits, label):
+        criterion = nn.CrossEntropyLoss(ignore_index=self.args.ignore_label)
+        loss = criterion(logits, label)
+        return loss
